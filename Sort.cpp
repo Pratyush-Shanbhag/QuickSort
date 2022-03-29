c7d209d0b9886975deaeda9655f0df73d7976388
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-29 13:57:31 -0700

    added file i/o to Sort class

--- Sort.cpp
+++ Sort.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include "Sort.h"
 
 using namespace std;
+
+void Sort::readFile(string fileName) {
+    ifstream infile(fileName);
+    int count = 0;
+    while(!infile.fail()) {
+        infile >> arr[count++];
+    }
+
+    infile.close();
+}
+
+void Sort::writeFile() {
+    ofstream outfile("SortedArray.txt");
+
+    for(int i = 0; i < 10; i++) {
+        outfile << arr[i] << endl;
+    }
+}
 
 void Sort::displayArray() {
     cout << "Array:\n[";
--- Sort.h
+++ Sort.h
@@ -1,11 +1,15 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <string>
+
 class Sort {
     public:
         int arr[10];
     
     public:
+        void readFile(string fileName);
+        void writeFile();
         virtual void sort() = 0;
         void displayArray();
 };
